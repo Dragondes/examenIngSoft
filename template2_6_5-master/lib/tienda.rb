5fcf3105f22e72db0b99a12fc67190fff619285c
Unknown <cevs12321@gmail.com>
2020-12-01 09:20:13 -0400

    tercera prueba

--- template2_6_5-master/lib/tienda.rb
+++ template2_6_5-master/lib/tienda.rb
@@ -1,6 +1,7 @@
 class Tienda
 
-    def calcular_Precio_Final(precio)
-        return precioFinal = precio
+    def calcular_Precio_Final(cantidad_items,precio_unitario)
+        precioFinal = precio_unitario*cantidad_items
+        return precioFinal
     end
 end
--- template2_6_5-master/spec/tienda_spec.rb
+++ template2_6_5-master/spec/tienda_spec.rb
@@ -5,9 +5,12 @@ RSpec.describe Tienda do
     
     before { @tienda = Tienda.new } #arrange
     it 'prueba inicial deberia devolver 30' do
-        expect(@tienda.calcular_Precio_Final(30)).to eq(30)
+        expect(@tienda.calcular_Precio_Final(1,30)).to eq(30)
     end
     it 'segunda prueba deberia devolver el precio ingresado' do
-        expect(@tienda.calcular_Precio_Final(40)).to eq(40)
+        expect(@tienda.calcular_Precio_Final(1,40)).to eq(40)
+    end
+    it 'tercera prueba deberia devolver el precio ingresado' do
+        expect(@tienda.calcular_Precio_Final(100,40)).to eq(4000)
     end
 end
