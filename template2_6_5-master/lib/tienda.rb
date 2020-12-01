ec3be12bc6dda0bf829efc29c6b3a8e1519c4735
Unknown <cevs12321@gmail.com>
2020-12-01 09:30:26 -0400

    cuarta prueba

--- template2_6_5-master/lib/tienda.rb
+++ template2_6_5-master/lib/tienda.rb
@@ -1,7 +1,10 @@
 class Tienda
 
+
     def calcular_Precio_Final(cantidad_items,precio_unitario)
-        precioFinal = precio_unitario*cantidad_items
+            precioFinal = precio_unitario*cantidad_items
+            descuento = 0.03
+            precioFinal = precioFinal * descuento
         return precioFinal
     end
 end
--- template2_6_5-master/spec/tienda_spec.rb
+++ template2_6_5-master/spec/tienda_spec.rb
@@ -4,13 +4,16 @@ require 'tienda.rb'
 RSpec.describe Tienda do
     
     before { @tienda = Tienda.new } #arrange
-    it 'prueba inicial deberia devolver 30' do
-        expect(@tienda.calcular_Precio_Final(1,30)).to eq(30)
-    end
-    it 'segunda prueba deberia devolver el precio ingresado' do
-        expect(@tienda.calcular_Precio_Final(1,40)).to eq(40)
-    end
+    #it 'prueba inicial deberia devolver 30' do
+    #    expect(@tienda.calcular_Precio_Final(1,30)).to eq(30)
+    #end
+    #it 'segunda prueba deberia devolver el precio ingresado' do
+    #    expect(@tienda.calcular_Precio_Final(1,40)).to eq(40)
+    #end
+    #it 'tercera prueba deberia devolver el precio ingresado' do
+    #    expect(@tienda.calcular_Precio_Final(100,40)).to eq(4000)
+    #end
     it 'tercera prueba deberia devolver el precio ingresado' do
-        expect(@tienda.calcular_Precio_Final(100,40)).to eq(4000)
+        expect(@tienda.calcular_Precio_Final(1000,30)).to eq(900)
     end
 end
